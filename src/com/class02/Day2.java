1ad1789e794d62d8d7e66891ef410698908e7842
tohasukhoterin <toha_sukhoterin@yahoo.com>
2020-03-17 21:12:30 -0400

    If statement added to class Day2

--- src/com/class02/Day2.java
+++ src/com/class02/Day2.java
@@ -3,7 +3,10 @@ package com.class02;
 public class Day2 {
 
 	public static void main(String[] args) {
-
+		boolean isRain=true;
+		if(isRain) {
+			System.out.println("It is raining");
+		}
 
 	}
 
